04e8b1e9611118aebd4047f5b0eed15ae6993228
avattoly <avattoly@cisco.com>
2025-01-22 13:24:38 +0530

    addressing comment

--- features/steps/elephant_flow.py
+++ features/steps/elephant_flow.py
@@ -1,61 +1,21 @@
 from features.steps.cdo_apis import get_insights
 from behave import *
+import json
 
-
-@step('check elephant flow enhanced insight data')
+@step('check elephant flow insight data')
 def step_impl(context):
     insight = context.matched_insight
-    if(len(insight["data"]["flows"])!=1):
-        print(f"Expected 1 flow but got {len(insight['data']['flows'])}")
+    expected_flow_data = json.loads(context.text)["flows"]
+    expected_length = len(expected_flow_data)
+    if(len(insight["data"]["flows"])!=expected_length):
+        print(f"Expected {expected_length} flow but got {len(insight['data']['flows'])}")
         assert False
-    flow = insight["data"]["flows"][0]
+    flows = insight["data"]["flows"]
     
-    expected_flow_data = {
-        "sourceIp": "10.10.0.98",
-        "destinationIp": "20.20.0.98",
-        "sourcePort": 43000,
-        "destinationPort": 98,
-        "protocol": 6,  
-        "application": {
-                        "id": 929,
-                        "name": "YouTube",
-                        "description": "A video-sharing website on which users can upload, share, and view videos.",
-                        "riskIndex": 4
-                    }
-    }
-
-    del flow["stats"]
-    if(flow != expected_flow_data):
+    # remove timeseries data before assertion
+    for flow in flows:
+        del flow["stats"]
+    
+    if(flows != expected_flow_data):
         print(f"Expected flow data {expected_flow_data} but got {flow}")
         assert False
-    
-
-
-@step('check elephant flow basic insight data')
-def step_impl(context):
-    insight = context.matched_insight
-    if(len(insight["data"]["flows"])!=1):
-        print(f"Expected 1 flow but got {len(insight['data']['flows'])}")
-        assert False
-    flow = insight["data"]["flows"][0]
-    
-    expected_flow_data = {
-        "sourceIp": "10.10.0.98",
-        "destinationIp": "20.20.1.98",
-        "sourcePort": 43000,
-        "destinationPort": 99,
-        "protocol": 6,  
-        "application": {
-                    "id": 0,
-                    "name": None,
-                    "description": None,
-                    "riskIndex": 0
-                }
-    }
-
-    del flow["stats"]
-    if(flow != expected_flow_data):
-        print(f"Expected flow data {expected_flow_data} but got {flow}")
-        assert False
-    
-
