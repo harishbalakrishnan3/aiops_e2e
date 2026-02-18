99e5f1e32e51db43174f4924b6dbd21b7400b931
Harish Balakrishnan <harishbalakrishnan3@gmail.com>
2026-02-18 08:21:52 +0530

    Fixing the GET URL for RAVPN gateways to retrieve more devices

--- features/environment.py
+++ features/environment.py
@@ -141,7 +141,7 @@ def discover_ravpn_devices(context):
             "commands": [
                 {
                     "method": "GET",
-                    "link": "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/health/ravpngateways",
+                    "link": "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/health/ravpngateways?limit=200",
                     "body": "",
                 }
             ]
